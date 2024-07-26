3f9708d0b8e45866a2ff9660aad630dca90b8797
shgawa13 <37065523+shgawa13@users.noreply.github.com>
2024-07-26 03:12:18 +0300

    Adding local driving license functions

--- DVLD/DVLD/Licenses/frmAddUpdateLocalDrivingLicenseApplication.cs
+++ DVLD/DVLD/Licenses/frmAddUpdateLocalDrivingLicenseApplication.cs
@@ -12,11 +12,24 @@ namespace DVLD.Licenses
 {
    public partial class frmAddUpdateLocalDrivingLicenseApplication : Form
    {
+      public enum enMode { AddNew =0, Update=1}
+      private enMode _Mode = enMode.AddNew;
+      private int _PersonID;
+
       public frmAddUpdateLocalDrivingLicenseApplication()
       {
          InitializeComponent();
+         _Mode = enMode.AddNew;
       }
 
-      
+      public frmAddUpdateLocalDrivingLicenseApplication(int PersonID)
+      {
+         InitializeComponent();
+         _PersonID = PersonID;
+         _Mode = enMode.AddNew;
+      }
+
+
+
    }
 }

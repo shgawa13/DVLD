7230ea594cb84b8e7a38fa93683933fa53026196
shgawa13 <37065523+shgawa13@users.noreply.github.com>
2024-08-12 16:11:07 +0300

    change license control to Person control

--- DVLD/DVLD/Licenses/frmShowPersonLicenseHistory.Designer.cs
+++ DVLD/DVLD/Licenses/frmShowPersonLicenseHistory.Designer.cs
@@ -29,7 +29,6 @@
       private void InitializeComponent()
       {
          this.lblTitle = new System.Windows.Forms.Label();
-         this.ctrlDriverLicenseInfoWithFilter1 = new DVLD.Licenses.Local_License.controls.ctrlDriverLicenseInfoWithFilter();
          this.pictureBox1 = new System.Windows.Forms.PictureBox();
          this.tabControl1 = new System.Windows.Forms.TabControl();
          this.tabpLocal = new System.Windows.Forms.TabPage();
@@ -39,6 +38,7 @@
          this.dgvInternationalLicenses = new System.Windows.Forms.DataGridView();
          this.label1 = new System.Windows.Forms.Label();
          this.label2 = new System.Windows.Forms.Label();
+         this.ctrlPersonInfoWithFilter1 = new DVLD.People.Controls.ctrlPersonInfoWithFilter();
          ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
          this.tabControl1.SuspendLayout();
          this.tabpLocal.SuspendLayout();
@@ -54,20 +54,11 @@
          this.lblTitle.BackColor = System.Drawing.Color.Transparent;
          this.lblTitle.Font = new System.Drawing.Font("Franklin Gothic Medium", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
          this.lblTitle.ForeColor = System.Drawing.Color.WhiteSmoke;
-         this.lblTitle.Location = new System.Drawing.Point(430, 7);
+         this.lblTitle.Location = new System.Drawing.Point(493, 7);
          this.lblTitle.Name = "lblTitle";
          this.lblTitle.Size = new System.Drawing.Size(185, 30);
          this.lblTitle.TabIndex = 50;
          this.lblTitle.Text = "License History";
-         // 
-         // ctrlDriverLicenseInfoWithFilter1
-         // 
-         this.ctrlDriverLicenseInfoWithFilter1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(37)))), ((int)(((byte)(73)))));
-         this.ctrlDriverLicenseInfoWithFilter1.FilterEnabled = true;
-         this.ctrlDriverLicenseInfoWithFilter1.Location = new System.Drawing.Point(188, 37);
-         this.ctrlDriverLicenseInfoWithFilter1.Name = "ctrlDriverLicenseInfoWithFilter1";
-         this.ctrlDriverLicenseInfoWithFilter1.Size = new System.Drawing.Size(882, 454);
-         this.ctrlDriverLicenseInfoWithFilter1.TabIndex = 51;
          // 
          // pictureBox1
          // 
@@ -177,14 +168,24 @@
          this.label2.TabIndex = 55;
          this.label2.Text = "[???]";
          // 
+         // ctrlPersonInfoWithFilter1
+         // 
+         this.ctrlPersonInfoWithFilter1.BackColor = System.Drawing.Color.Gainsboro;
+         this.ctrlPersonInfoWithFilter1.FilterEnable = true;
+         this.ctrlPersonInfoWithFilter1.Location = new System.Drawing.Point(194, 40);
+         this.ctrlPersonInfoWithFilter1.Name = "ctrlPersonInfoWithFilter1";
+         this.ctrlPersonInfoWithFilter1.ShowAddPerson = true;
+         this.ctrlPersonInfoWithFilter1.Size = new System.Drawing.Size(968, 463);
+         this.ctrlPersonInfoWithFilter1.TabIndex = 55;
+         // 
          // frmShowPersonLicenseHistory
          // 
          this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(34)))), ((int)(((byte)(71)))));
-         this.ClientSize = new System.Drawing.Size(1082, 696);
+         this.ClientSize = new System.Drawing.Size(1161, 696);
+         this.Controls.Add(this.ctrlPersonInfoWithFilter1);
          this.Controls.Add(this.pictureBox1);
-         this.Controls.Add(this.ctrlDriverLicenseInfoWithFilter1);
          this.Controls.Add(this.lblTitle);
          this.Controls.Add(this.groupBox1);
          this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
@@ -206,7 +207,6 @@
       #endregion
 
       private System.Windows.Forms.Label lblTitle;
-      private Local_License.controls.ctrlDriverLicenseInfoWithFilter ctrlDriverLicenseInfoWithFilter1;
       private System.Windows.Forms.PictureBox pictureBox1;
       private System.Windows.Forms.TabControl tabControl1;
       private System.Windows.Forms.TabPage tabpLocal;
@@ -216,5 +216,6 @@
       private System.Windows.Forms.DataGridView dgvInternationalLicenses;
       private System.Windows.Forms.Label label1;
       private System.Windows.Forms.Label label2;
+      private People.Controls.ctrlPersonInfoWithFilter ctrlPersonInfoWithFilter1;
    }
 }

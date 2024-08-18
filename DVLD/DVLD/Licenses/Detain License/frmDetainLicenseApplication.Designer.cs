153bb64518bfb9fc64c39d429f1e242838b38fbc
shgawa13 <37065523+shgawa13@users.noreply.github.com>
2024-08-18 10:05:40 +0300

    Adding detain license form

---
+++ DVLD/DVLD/Licenses/Detain License/frmDetainLicenseApplication.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace DVLD.Licenses.Detain_License
+{
+   partial class frmDetainLicenseApplication
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.ctrlDriverLicenseInfoWithFilter1 = new DVLD.Licenses.Local_License.controls.ctrlDriverLicenseInfoWithFilter();
+         this.groupBox1 = new System.Windows.Forms.GroupBox();
+         this.lblApplicationID = new System.Windows.Forms.Label();
+         this.label1 = new System.Windows.Forms.Label();
+         this.label2 = new System.Windows.Forms.Label();
+         this.label3 = new System.Windows.Forms.Label();
+         this.label4 = new System.Windows.Forms.Label();
+         this.label5 = new System.Windows.Forms.Label();
+         this.label6 = new System.Windows.Forms.Label();
+         this.label7 = new System.Windows.Forms.Label();
+         this.label8 = new System.Windows.Forms.Label();
+         this.label9 = new System.Windows.Forms.Label();
+         this.groupBox1.SuspendLayout();
+         this.SuspendLayout();
+         // 
+         // ctrlDriverLicenseInfoWithFilter1
+         // 
+         this.ctrlDriverLicenseInfoWithFilter1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(37)))), ((int)(((byte)(73)))));
+         this.ctrlDriverLicenseInfoWithFilter1.FilterEnabled = true;
+         this.ctrlDriverLicenseInfoWithFilter1.Location = new System.Drawing.Point(1, -1);
+         this.ctrlDriverLicenseInfoWithFilter1.Name = "ctrlDriverLicenseInfoWithFilter1";
+         this.ctrlDriverLicenseInfoWithFilter1.Size = new System.Drawing.Size(882, 455);
+         this.ctrlDriverLicenseInfoWithFilter1.TabIndex = 0;
+         // 
+         // groupBox1
+         // 
+         this.groupBox1.BackColor = System.Drawing.Color.Gainsboro;
+         this.groupBox1.Controls.Add(this.label8);
+         this.groupBox1.Controls.Add(this.label9);
+         this.groupBox1.Controls.Add(this.label6);
+         this.groupBox1.Controls.Add(this.label7);
+         this.groupBox1.Controls.Add(this.label4);
+         this.groupBox1.Controls.Add(this.label5);
+         this.groupBox1.Controls.Add(this.label2);
+         this.groupBox1.Controls.Add(this.label3);
+         this.groupBox1.Controls.Add(this.lblApplicationID);
+         this.groupBox1.Controls.Add(this.label1);
+         this.groupBox1.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.groupBox1.ForeColor = System.Drawing.Color.Crimson;
+         this.groupBox1.Location = new System.Drawing.Point(1, 452);
+         this.groupBox1.Name = "groupBox1";
+         this.groupBox1.Size = new System.Drawing.Size(882, 133);
+         this.groupBox1.TabIndex = 1;
+         this.groupBox1.TabStop = false;
+         this.groupBox1.Text = "Detain Info";
+         // 
+         // lblApplicationID
+         // 
+         this.lblApplicationID.AutoSize = true;
+         this.lblApplicationID.BackColor = System.Drawing.Color.Gainsboro;
+         this.lblApplicationID.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.lblApplicationID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(47)))), ((int)(((byte)(88)))));
+         this.lblApplicationID.Location = new System.Drawing.Point(155, 22);
+         this.lblApplicationID.Name = "lblApplicationID";
+         this.lblApplicationID.Size = new System.Drawing.Size(49, 21);
+         this.lblApplicationID.TabIndex = 89;
+         this.lblApplicationID.Text = "[???]";
+         // 
+         // label1
+         // 
+         this.label1.AutoSize = true;
+         this.label1.BackColor = System.Drawing.Color.Gainsboro;
+         this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(47)))), ((int)(((byte)(88)))));
+         this.label1.Location = new System.Drawing.Point(35, 22);
+         this.label1.Name = "label1";
+         this.label1.Size = new System.Drawing.Size(87, 21);
+         this.label1.TabIndex = 88;
+         this.label1.Text = "Detain ID:";
+         // 
+         // label2
+         // 
+         this.label2.AutoSize = true;
+         this.label2.BackColor = System.Drawing.Color.Gainsboro;
+         this.label2.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(47)))), ((int)(((byte)(88)))));
+         this.label2.Location = new System.Drawing.Point(155, 52);
+         this.label2.Name = "label2";
+         this.label2.Size = new System.Drawing.Size(49, 21);
+         this.label2.TabIndex = 91;
+         this.label2.Text = "[???]";
+         // 
+         // label3
+         // 
+         this.label3.AutoSize = true;
+         this.label3.BackColor = System.Drawing.Color.Gainsboro;
+         this.label3.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(47)))), ((int)(((byte)(88)))));
+         this.label3.Location = new System.Drawing.Point(35, 52);
+         this.label3.Name = "label3";
+         this.label3.Size = new System.Drawing.Size(107, 21);
+         this.label3.TabIndex = 90;
+         this.label3.Text = "Detain Date:";
+         // 
+         // label4
+         // 
+         this.label4.AutoSize = true;
+         this.label4.BackColor = System.Drawing.Color.Gainsboro;
+         this.label4.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(47)))), ((int)(((byte)(88)))));
+         this.label4.Location = new System.Drawing.Point(155, 82);
+         this.label4.Name = "label4";
+         this.label4.Size = new System.Drawing.Size(49, 21);
+         this.label4.TabIndex = 93;
+         this.label4.Text = "[???]";
+         // 
+         // label5
+         // 
+         this.label5.AutoSize = true;
+         this.label5.BackColor = System.Drawing.Color.Gainsboro;
+         this.label5.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(47)))), ((int)(((byte)(88)))));
+         this.label5.Location = new System.Drawing.Point(35, 82);
+         this.label5.Name = "label5";
+         this.label5.Size = new System.Drawing.Size(88, 21);
+         this.label5.TabIndex = 92;
+         this.label5.Text = "Fine Fees:";
+         // 
+         // label6
+         // 
+         this.label6.AutoSize = true;
+         this.label6.BackColor = System.Drawing.Color.Gainsboro;
+         this.label6.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(47)))), ((int)(((byte)(88)))));
+         this.label6.Location = new System.Drawing.Point(496, 22);
+         this.label6.Name = "label6";
+         this.label6.Size = new System.Drawing.Size(49, 21);
+         this.label6.TabIndex = 95;
+         this.label6.Text = "[???]";
+         // 
+         // label7
+         // 
+         this.label7.AutoSize = true;
+         this.label7.BackColor = System.Drawing.Color.Gainsboro;
+         this.label7.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(47)))), ((int)(((byte)(88)))));
+         this.label7.Location = new System.Drawing.Point(347, 22);
+         this.label7.Name = "label7";
+         this.label7.Size = new System.Drawing.Size(94, 21);
+         this.label7.TabIndex = 94;
+         this.label7.Text = "License ID:";
+         // 
+         // label8
+         // 
+         this.label8.AutoSize = true;
+         this.label8.BackColor = System.Drawing.Color.Gainsboro;
+         this.label8.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(47)))), ((int)(((byte)(88)))));
+         this.label8.Location = new System.Drawing.Point(496, 52);
+         this.label8.Name = "label8";
+         this.label8.Size = new System.Drawing.Size(49, 21);
+         this.label8.TabIndex = 97;
+         this.label8.Text = "[???]";
+         // 
+         // label9
+         // 
+         this.label9.AutoSize = true;
+         this.label9.BackColor = System.Drawing.Color.Gainsboro;
+         this.label9.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(47)))), ((int)(((byte)(88)))));
+         this.label9.Location = new System.Drawing.Point(347, 52);
+         this.label9.Name = "label9";
+         this.label9.Size = new System.Drawing.Size(99, 21);
+         this.label9.TabIndex = 96;
+         this.label9.Text = "Created By:";
+         // 
+         // frmDetainLicenseApplication
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(881, 585);
+         this.Controls.Add(this.groupBox1);
+         this.Controls.Add(this.ctrlDriverLicenseInfoWithFilter1);
+         this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+         this.Name = "frmDetainLicenseApplication";
+         this.Text = "Detain License Application";
+         this.groupBox1.ResumeLayout(false);
+         this.groupBox1.PerformLayout();
+         this.ResumeLayout(false);
+
+      }
+
+      #endregion
+
+      private Local_License.controls.ctrlDriverLicenseInfoWithFilter ctrlDriverLicenseInfoWithFilter1;
+      private System.Windows.Forms.GroupBox groupBox1;
+      private System.Windows.Forms.Label label8;
+      private System.Windows.Forms.Label label9;
+      private System.Windows.Forms.Label label6;
+      private System.Windows.Forms.Label label7;
+      private System.Windows.Forms.Label label4;
+      private System.Windows.Forms.Label label5;
+      private System.Windows.Forms.Label label2;
+      private System.Windows.Forms.Label label3;
+      private System.Windows.Forms.Label lblApplicationID;
+      private System.Windows.Forms.Label label1;
+   }
+}
